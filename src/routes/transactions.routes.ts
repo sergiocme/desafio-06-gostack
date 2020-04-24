28c200eaf2d259462b656c61e356eb8c89796b2d
Sérgio Carvalho <sergiocarvalho.m@outlook.com>
2020-04-24 12:31:33 -0300

    Implement delete transaction

--- src/routes/transactions.routes.ts
+++ src/routes/transactions.routes.ts
@@ -3,7 +3,7 @@ import { Router } from 'express';
 import { getCustomRepository } from 'typeorm';
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import CreateTransactionService from '../services/CreateTransactionService';
-// import DeleteTransactionService from '../services/DeleteTransactionService';
+import DeleteTransactionService from '../services/DeleteTransactionService';
 // import ImportTransactionsService from '../services/ImportTransactionsService';
 
 const transactionsRouter = Router();
@@ -35,7 +35,9 @@ transactionsRouter.post('/', async (request, response) => {
 });
 
 transactionsRouter.delete('/:id', async (request, response) => {
-  response.send({});
+  const deleteTransactionService = new DeleteTransactionService();
+  await deleteTransactionService.execute(request.params.id);
+  return response.status(204).send();
 });
 
 transactionsRouter.post('/import', async (request, response) => {
